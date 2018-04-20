e1f07aae7f3263429f31a87abc6a37c53b293e63
Fedya Trubitsyn <trubitsyn.fedya@yandex.ru>
2018-04-20 14:28:40 +0200

    [!] Dependencies fixed;

--- extensions/src/main/java/com/fedka27/github/lib/kotlin_extentions/ContextExtensions.kt
+++ extensions/src/main/java/com/fedka27/github/lib/kotlin_extentions/ContextExtensions.kt
@@ -2,6 +2,7 @@ package com.fedka27.github.lib.kotlin_extentions
 
 import android.app.ActivityManager
 import android.content.Context
+import android.support.v4.app.NotificationManagerCompat
 
 fun Context.appInForeground(): Boolean {
     val activityManager = this.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
